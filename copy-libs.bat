e98e1667c0b79bb94f346e7213895d8e35d8ded0
Jonathan Sharman <jonathan.sharman@gmail.com>
2020-02-11 23:03:03 -0600

    Can now somewhat easily generate x86/x64 and Release/Debug versions of card-gen.lib.

---
+++ copy-libs.bat
@@ -0,0 +1,4 @@
+copy Win32\Debug\card-gen.lib lib\x86\debug\card-gen.lib
+copy Win32\Release\card-gen.lib lib\x86\card-gen.lib
+copy x64\Debug\card-gen.lib lib\x64\debug\card-gen.lib
+copy x64\Release\card-gen.lib lib\x64\card-gen.lib
